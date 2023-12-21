bc6cffe730c27c8327254f20caf922a3cde45ed8
David <davidbazanticse@gmail.com>
2023-12-21 17:16:14 +0000

    Add 3 main input fields/dropdowns (campus, building, room)

--- app/src/main/java/com/david/qmul_room_timetable_app/AddRoomTimetable.kt
+++ app/src/main/java/com/david/qmul_room_timetable_app/AddRoomTimetable.kt
@@ -1,10 +1,15 @@
 package com.david.qmul_room_timetable_app
 
+import android.content.DialogInterface
 import android.os.Bundle
+import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
+import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.david.qmul_room_timetable_app.model.Building
 import com.david.qmul_room_timetable_app.model.Campus
 import com.david.qmul_room_timetable_app.util.JsonParser
 
@@ -13,6 +18,13 @@ class AddRoomTimetable : AppCompatActivity() {
     private lateinit var campusList: List<Campus>
     private lateinit var adapterBuilding: ArrayAdapter<String>
     private lateinit var adapterCampus: ArrayAdapter<String>
+    private lateinit var textViewRooms: TextView
+    private lateinit var selectedCampus: Campus
+    private lateinit var selectedBuilding: Building
+    private lateinit var selectedRooms: Array<String>
+    private lateinit var selectedRoomsBoolean: BooleanArray
+    private lateinit var roomsList: ArrayList<Int>
+    private lateinit var roomsArray: Array<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,19 +35,23 @@ class AddRoomTimetable : AppCompatActivity() {
 
         val autoCompleteTextViewCampus = findViewById<AutoCompleteTextView>(R.id.autoCompleteTextView2)
         val autoCompleteTextViewBuilding = findViewById<AutoCompleteTextView>(R.id.autoCompleteTextView5)
-
-
+        textViewRooms = findViewById(R.id.textView)
         adapterCampus = ArrayAdapter(this, R.layout.dropdown_item, getCampusNames())
         adapterBuilding = ArrayAdapter(this, R.layout.dropdown_item, mutableListOf<String>())
-
 
         autoCompleteTextViewCampus.setAdapter(adapterCampus)
         autoCompleteTextViewBuilding.setAdapter(adapterBuilding)
 
         autoCompleteTextViewCampus.setOnItemClickListener { _, _, position, _ ->
-            val selectedCampus = campusList[position]
+            selectedCampus = campusList[position]
             Toast.makeText(this, "Item: ${selectedCampus.campus}", Toast.LENGTH_SHORT).show()
             updateBuildingDropdown(selectedCampus.buildings.map { it.building })
+        }
+
+        autoCompleteTextViewBuilding.setOnItemClickListener { _, _, position, _ ->
+            selectedBuilding = selectedCampus.buildings[position]
+            Toast.makeText(this, "Item: ${selectedBuilding.building}", Toast.LENGTH_SHORT).show()
+            updateRoomDropdown();
         }
     }
 
@@ -44,9 +60,60 @@ class AddRoomTimetable : AppCompatActivity() {
     }
 
     private fun updateBuildingDropdown(buildingNames: List<String>) {
-        if (!adapterBuilding.isEmpty) adapterBuilding.clear()
+        adapterBuilding.clear()
         adapterBuilding.addAll(buildingNames)
         adapterBuilding.notifyDataSetChanged()
+    }
+
+    private fun updateRoomDropdown() {
+        roomsArray = selectedBuilding.rooms.toTypedArray();
+        roomsList = ArrayList()
+        selectedRoomsBoolean = BooleanArray(roomsArray.size)
+    }
+
+    fun showRoomsDialog(view: View) {
+        var builder: AlertDialog.Builder = AlertDialog.Builder(this)
+
+        builder.setTitle("Select Room(s)")
+        builder.setCancelable(false)
+
+        builder.setMultiChoiceItems(roomsArray, selectedRoomsBoolean
+        ) { dialog: DialogInterface, which: Int, isChecked: Boolean ->
+            if (isChecked) {
+                roomsList.add(which)
+            } else {
+                roomsList.remove(which)
+            }
+        }
+
+        builder.setPositiveButton("OK"
+        ) {dialog, i ->
+            val stringBuilder: StringBuilder = StringBuilder()
+
+            for (j in 0 until roomsList.size) {
+                stringBuilder.append(roomsArray[roomsList[j]])
+
+                if (j != roomsList.size - 1) {
+                    stringBuilder.append(", ")
+                }
+            }
+
+            textViewRooms.text = stringBuilder.toString()
+        }
+
+        builder.setNegativeButton("Cancel"
+        ) { dialog, i ->
+            dialog.dismiss()
+        }
+
+        builder.setNeutralButton("Clear All"
+        ) {dialog, i ->
+            selectedRoomsBoolean.fill(false)
+            roomsList.clear()
+            textViewRooms.text = ""
+        }
+
+        builder.show()
     }
 
 }
