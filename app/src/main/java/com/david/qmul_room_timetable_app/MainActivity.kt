dd10d5c03fef8f439b55127429f7e2aa1a5daddd
David <davidbazanticse@gmail.com>
2023-12-23 19:59:49 +0000

    #3, Finish serializer and add DataStore declaration

--- app/src/main/java/com/david/qmul_room_timetable_app/MainActivity.kt
+++ app/src/main/java/com/david/qmul_room_timetable_app/MainActivity.kt
@@ -1,9 +1,20 @@
 package com.david.qmul_room_timetable_app
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.datastore.core.DataStore
+import androidx.datastore.dataStore
+
+private const val ROOM_TIMETABLE_QUERY_NAME = "room_timetable_query"
+private const val DATA_STORE_FILE_NAME = "room_timetable_queries.pb"
+
+private val Context.roomTimetableQueryStore: DataStore<RoomTimetableQuery> by dataStore(
+    fileName = DATA_STORE_FILE_NAME,
+    serializer = RoomTimetableQuerySerializer
+)
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/david/qmul_room_timetable_app/RoomTimetableQuerySerializer.kt
+++ app/src/main/java/com/david/qmul_room_timetable_app/RoomTimetableQuerySerializer.kt
@@ -1,19 +1,25 @@
 package com.david.qmul_room_timetable_app
 
+import androidx.datastore.core.CorruptionException
 import androidx.datastore.core.Serializer
+import com.google.protobuf.InvalidProtocolBufferException
 import java.io.InputStream
 import java.io.OutputStream
 
-object RoomTimetableQuerySerializer : Serializer<RoomTimetableQuerySerializer> {
+object RoomTimetableQuerySerializer : Serializer<RoomTimetableQuery> {
 
     override val defaultValue: RoomTimetableQuery = RoomTimetableQuery.getDefaultInstance()
 
+//    @Suppress("BlockingMethodInNonBlockingContext")
     override suspend fun readFrom(input: InputStream): RoomTimetableQuery {
-        TODO("Not yet implemented")
+        try {
+            return RoomTimetableQuery.parseFrom(input)
+        } catch (exception: InvalidProtocolBufferException) {
+            throw CorruptionException("Cannot read proto.", exception)
+        }
     }
 
-    override suspend fun writeTo(t: RoomTimetableQuerySerializer, output: OutputStream) {
-        TODO("Not yet implemented")
-    }
+//    @Suppress("BlockingMethodInNonBlockingContext")
+    override suspend fun writeTo(t: RoomTimetableQuery, output: OutputStream) = t.writeTo(output)
 
 }
